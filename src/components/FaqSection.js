91e4f61a6b893a21651b54fd4b2672704300fbd7
Emma <imgenu@gmail.com>
2021-11-02 23:57:59 +0800

    7. styling Faq Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { About } from "../styles";
+import styled from "styled-components";
 
 const FAQComponent = ({ questionText }) => {
   return (
@@ -10,12 +12,13 @@ const FAQComponent = ({ questionText }) => {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, modi.
         </p>
       </div>
+      <div className="faq-line"></div>
     </div>
   );
 };
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any question<span>FAQ</span>
       </h2>
@@ -23,7 +26,34 @@ const FaqSection = () => {
       <FAQComponent questionText={"Daily Schedule"} />
       <FAQComponent questionText={"Different Payment Methods"} />
       <FAQComponent questionText={"What Product Do You Offer."} />
-    </div>
+    </Faq>
   );
 };
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
+
 export default FaqSection;
